28b9d9cdd869522fa05613c89cba2e5b2fef5c63
walter <berwal50@gmail.com>
2021-02-16 18:27:06 +0100

    buttons hinzugefügt

--- src/window.py
+++ src/window.py
@@ -15,8 +15,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Gtk Imports
+
+import gi
+
+gi.require_version('Gtk', '3.0')
+
 from gi.repository import Gtk
-from gi.repository import Gdk, GObject
+from gi.repository import Gdk, Gio
+from gi.repository import GLib
 
 # Python Imports
 import cairo
@@ -29,16 +36,58 @@ class OriginalWindow(Gtk.ApplicationWindow):
 
     drawArea = Gtk.Template.Child()
     anZeige  = Gtk.Template.Child()
+    quadranten  = Gtk.Template.Child()
+    neustarten  = Gtk.Template.Child()
+
+
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.drawArea.set_events(Gdk.EventMask.ALL_EVENTS_MASK)
 
+        self.drawArea.connect('draw', self.onDraw)
+        self.drawArea.connect('configure-event', self.onConfigure)
         self.drawArea.connect("button-press-event", self.onButtonPress)
         self.drawArea.connect("motion-notify-event", self.zeigeKoord)
+        self.quadranten.connect("clicked", self.einVierQuad)
+        self.neustarten.connect("clicked", self.neuStart)
+
+        self.quadra = 2
+        self.zeichneneu  = True
+        self.surface     = None
+        self.aktBreite   = 0
+        self.aktHoehe    = 0
+        self.crFarbe     = [1.0, 0.0, 0.0, 1.0]
+        self.crDicke     = 4
+
 
     def onButtonPress(self, event, widget):
         print ("Bonege!")
+
+    def einVierQuad(self, event, widget):
+        if self.quadra == 2:
+            self.quadra = 8
+        elif self.quadra == 8:
+            self.quadra = 2
+        else:
+            print ("self.quadra muss 2 oder 8 sein!")
+
+    def neuStart(self, widget):
+        sb = self.surface.get_width()
+        sh = self.surface.get_height()
+        print("sh, sb", sh, sb)
+        self.cr.rectangle(0, 0, sb, sh)  # x, y, width, height
+        self.cr.set_operator(0);
+        self.cr.fill()
+        print("sh, sb", sh, sb)
+
+        self.cr.set_operator(1)
+        self.linio(0, pha, sb, pha) # zeichnet die horizontale Achse
+        self.linio(pva, 0, pva, sh)
+        print("sh, sb", sh, sb)
+
+        self.drawArea.queue_draw()
+
 
     def zeigeKoord (self, area, eve):
         aw = area.get_allocated_width()   #liest die aktuellen Abmessungen des Fensters ein
@@ -46,20 +95,98 @@ class OriginalWindow(Gtk.ApplicationWindow):
 
         self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, aw, ah)
 
-        sw = self.surface.get_width()
+        sb = self.surface.get_width()
         sh = self.surface.get_height()
         (x1, y1) = eve.x, eve.y
-        x = int(x1-sw/2)         # x und y sind die Koordinaten im Aschsenkreuz der Zeichenebene
-        y = int(-y1+sh/2)
+
+        x = int(x1-pva)         # x und y sind die Koordinaten im Aschsenkreuz der Zeichenebene
+        y = int(-y1+pha)
         text = "(x = " + str(x) + ", y = " + str(y) + ")"
         print (text)
 
-        koordinat  = Gtk.Label(text)
-        self.anZeige.add(koordinat)
-        self.anZeige.show_all()
-        time.sleep(0.5)
-        self.anZeige.queue_draw()
-        self.anZeige.show_all()
+        self.anZeige.set_text(text)
 
+    def onConfigure(self, area, eve, data = None): # wird bei Änderung des Fensters aufgerufen
+        ab = area.get_allocated_width()   #liest die aktuellen Abmessungen des Fensters ein
+        ah = area.get_allocated_height()
+
+        _surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, ab, ah)
+
+        # wenn das Fenster verändert wird, wird die Zeicchenebene angepasst
+        if self.surface is not None:
+            global sb, sh
+            sb = self.surface.get_width()
+            sh = self.surface.get_height()
+
+            global pva, pha         # Position der vertikalen/horizontalen Achse
+            pva = sb/self.quadra
+            pha = sh - sh/self.quadra
+
+            # bei Verkleinerung bleibt die alte Zeichenebene
+            if ab < sb and ah < sh:
+                return False
+
+            cr = cairo.Context(_surface)
+
+            # lädt die vorherige Ebene
+            cr.set_source_surface(self.surface, 0.0, 0.0)
+            cr.scale(ab, ah)
+            cr.paint()
+
+            self.aktBreite   = ab
+            self.aktHoehe  = ah
+
+            self.zeichneneu    = True     # d.h. dass in onDraw die Zeichenfläche neu gezeichnet wird
+
+
+        else:
+            self.drawArea.set_size_request(self.aktBreite, self.aktHoehe)
+
+        self.surface = _surface
+        self.cr   = cairo.Context(self.surface)
+        return False
+
+        # onDraw wird aufgerufen, wenn die Zeichebene neu gerendert wird
+    def onDraw(self, area, cr):    # area ist das Fenster  cr ist ein context
+        ab = area.get_allocated_width()   # liest die aktuellen Abmessungen des Fensters ein
+        ah = area.get_allocated_height()
+
+        if self.surface is not None:
+            cr.set_source_surface(self.surface, 0.0, 0.0)
+            cr.paint()
+
+            sb = self.surface.get_width()
+            sh = self.surface.get_height()
+
+            #print ("ab", ab, ah)
+            #print ("sb", sb, sh)
+            # bei Verkleinerung bleibt die alte Zeichenebene
+            if ab < sb and ah < sh:
+                return False
+
+            if self.zeichneneu:
+                print ("1pva =", pva, "pha =", pha)
+
+                self.cr.rectangle(0, 0, sb, sh)  # x, y, width, height
+                self.cr.set_operator(0);
+                self.cr.fill()
+                self.cr.set_operator(1)
+                self.linio(0, pha, sb, pha) # zeichnet die horizontale Achse
+                self.linio(pva, 0, pva, sh)
+
+                self.zeichneneu = False
+
+        else:
+            print ("Keine Infos über die Zeichenebene ...")
+
+        return False
+
+    def linio(self, x1, y1, x2, y2):
+        self.cr.move_to(x1, y1)
+        self.cr.line_to(x2, y2)
+
+        self.cr.set_source_rgb(0, 0, 0)
+        self.cr.set_line_width(1.5)
+        self.cr.stroke()
 
 
